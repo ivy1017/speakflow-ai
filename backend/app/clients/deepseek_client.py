f8362f31b4c3cea85f26483128f55c256f99630c
wangyingjing <1813634257@qq.com>
2026-05-22 17:46:10 +0800

    feat(be): 完成语音识别接口完整链路

---
+++ backend/app/clients/deepseek_client.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url=os.getenv("DEEPSEEK_BASE_URL"),
+)
---
+++ backend/app/services/chat_service.py
@@ -0,0 +1,29 @@
+from app.clients.deepseek_client import client
+
+
+def generate_reply(user_text: str):
+    try:
+        response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=[
+                {
+                    "role": "system",
+                    "content": """
+你是一个英语口语陪练老师。
+用英文回复。
+"""
+                },
+                {
+                    "role": "user",
+                    "content": user_text
+                }
+            ]
+        )
+
+        print(response)
+
+        return response.choices[0].message.content
+
+    except Exception as e:
+        print("DeepSeek 调用失败:", e)
+        return "Sorry, AI is temporarily unavailable."
